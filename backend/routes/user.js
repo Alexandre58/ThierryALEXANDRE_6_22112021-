d5f2c82f21a43597fc68a8421a3af277f1fdb5d0
cherokeeal58 <cherokee.al58@outlook.com>
2021-12-22 17:55:24 +0100

    password-validator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const userControle = require('../controllers/user');
+const password = require('../middleware/password');
 
-router.post('/signup', userControle.signup);
+router.post('/signup',password, userControle.signup);
 router.post('/login', userControle.login);
 
 
